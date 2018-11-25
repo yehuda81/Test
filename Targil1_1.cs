ab2b46306d8b1527d08cb337c90673356fc613c3
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-25 17:37:46 +0200

    Add files via upload

---
+++ Targil1_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a = Convert.ToInt32(Console.ReadLine());
+			
+			int m = 2;
+			while (a % m != 0 && m < a)
+			{
+				m++;
+			}
+			if (a == m || a == 1)
+			{
+				Console.WriteLine("This is Prime number");
+
+			}
+			else
+				Console.WriteLine("This is NOT a Prime number");
+			
+		}
+	}
+}
