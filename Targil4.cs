cfc7f608f4ab7ebf4ac0575d7f61dbb5c88f716e
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-25 19:22:46 +0200

    Add files via upload

---
+++ Targil4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			{
+				int stars = Convert.ToInt32(Console.ReadLine());
+				if (stars >= 1 && stars <= 15)
+				{
+					stars = (stars - 1) * 2 + 1;
+					int m = stars / 2 + 1;
+					int j = 1;
+					while (j <= stars && stars >= 1)
+					{
+						for (int i = 1; i < m; i++)
+						{
+							Console.Write(" ");
+						}
+						for (int i = 1; i <= j; i++)
+						{
+							Console.Write("*");
+						}
+						m--;
+						j = j + 2;
+						Console.WriteLine();
+					}
+				}
+				else
+				{
+					Console.WriteLine("wrong number");
+				}
+
+			}
+		}
+	}
+}
