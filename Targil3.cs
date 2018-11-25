58dbaecd43394769d3c6ddd5dbf68e63822e1b97
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-25 19:10:24 +0200

    Add files via upload

---
+++ Targil3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter a number:");
+			int number = Convert.ToInt32(Console.ReadLine());
+			for (int i = 1; i <= number; i++)
+			{
+				for (int j = 1; j <= i; j++)
+				{
+					Console.Write(j);
+				}
+
+				for (int j = i + 1; j <= number; j++)
+				{
+					Console.Write('*');
+				}
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
