2c1e5ffecc069040758cab596b9cf6d55a0b44ac
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2018-11-25 17:58:54 +0200

    Add files via upload

---
+++ Targil1_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil1_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter 3 numbers");
+			int a = Convert.ToInt32(Console.ReadLine());
+			int b = Convert.ToInt32(Console.ReadLine());
+			int c = Convert.ToInt32(Console.ReadLine());
+			int m = 2;
+			while (a % m != 0 && m < a)
+			{
+				m++;
+			}
+			if (a == m || a == 1)
+			{
+				m = 2;
+				while (b % m != 0 && m < b)
+				{
+					m++;
+				}
+				if (b == m || b == 1)
+				{
+					m = 2;
+					while (c % m != 0 && m < c)
+					{
+						m++;
+					}
+					if (c == m || c == 1)
+					{
+						int sum = a + b + c;
+						Console.WriteLine(sum);
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine("NOT a Prime number");
+			}
+
+				}
+				
+	}
+}
